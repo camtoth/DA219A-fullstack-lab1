301b5be8d0d18d63d1c30c8cae90915c23816b9a
Cameron Toth <smallpotato6035@gmail.com>
2023-04-20 19:45:12 +0200

    implement db connection

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyPaser= require('body-parser')
+const dotenv = require('dotenv').config()
+const mongoose = require("mongoose");
+
+// express setup
+app.use(express.json())
+app.use(bodyPaser.json())
+app.use(bodyPaser.urlencoded({ extended: true }));
+
+// database setup and connection
+mongoose.set("strictQuery", false);
+const mongodbURI = process.env.CONNECTIONSTRING
+mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>console.log('MongoDB and Mongoose connected!'))
+.catch(error =>console.error('Error in connection!',error))
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port: " + process.env.PORT);
+})
