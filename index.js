1824cc5a460d5828248dc88bf1172006207843ba
Cameron Toth <smallpotato6035@gmail.com>
2023-04-20 20:13:40 +0200

    implement retrieve data from db

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyPaser= require('body-parser')
 const dotenv = require('dotenv').config()
 const mongoose = require("mongoose");
 const Album = require('./src/Album')
+const path = require('path')
 
 // express setup
 app.use(express.json())
@@ -19,4 +20,23 @@ mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true }
 
 app.listen(process.env.PORT, () => {
     console.log("Listening on port: " + process.env.PORT);
+})
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.get('/albums', async(req,res)=>{
+    try {
+        const albums = await Album.find()
+        .exec()
+
+        const count= await Album.count()
+
+        res.json({
+            data:albums,
+        })
+    } catch (error) {
+        res.status(500).json({messege: "error in getting customers"})
+    }
 })
